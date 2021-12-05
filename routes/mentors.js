e33732b84b899233818432c60ed877d069511c0a
unknown <santhosh8857@gmail.com>
2021-12-05 21:30:12 +0530

    added mentor to student

--- routes/mentors.js
+++ routes/mentors.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require("bson");
 var express = require("express");
 const { MongoClient, dbURL } = require("../dbConfig");
 var router = express.Router();
@@ -30,6 +31,22 @@ router.post("/create-mentor", async (req, res) => {
     const data = await db.collection("mentors").insertOne(req.body);
 
     res.send({ message: "mentor created!!!", data: data });
+  } catch (err) {
+    console.log(err);
+    res.send({ message: "Error in connection" });
+  } finally {
+    client.close();
+  }
+});
+
+// assign mentor to mulitple students
+router.put("/assign-mentor/:id", async (req, res) => {
+  const client = await MongoClient.connect(dbURL);
+  try {
+    // const db = client.db("assign-mentor");
+    // const mentorData = await db
+    //   .collection("mentors")
+    //   .updateOne({ _id: ObjectId(req.params.id) }, { $push: {} });
   } catch (err) {
     console.log(err);
     res.send({ message: "Error in connection" });
--- routes/students.js
+++ routes/students.js
@@ -41,4 +41,56 @@ router.post("/create-student", async (req, res) => {
   }
 });
 
+// assign student to mentor
+router.put("/assign-mentor/:id", async (req, res) => {
+  const client = await MongoClient.connect(dbURL);
+
+  try {
+    const db = client.db("assign-mentor");
+
+    // to get the student details
+    const studentDetails = await db
+      .collection("students")
+      .findOne({ _id: ObjectId(req.params.id) });
+
+    // to get the mentor details to update the student array
+    const mentorDetails = await db
+      .collection("mentors")
+      .findOne({ name: req.body.mentor });
+
+    // to verify mentor is available
+    if (mentorDetails) {
+      // updating student collection
+      let find = mentorDetails.students.indexOf(`${studentDetails.name}`);
+      if (find === -1) {
+        const studentData = await db
+          .collection("students")
+          .updateOne(
+            { _id: ObjectId(req.params.id) },
+            { $set: { mentor: req.body.mentor } }
+          );
+
+        // updating mentor collection
+        const mentorData = await db.collection("mentors").updateOne(
+          { name: req.body.mentor },
+          // pushing the student name to the mentor collection student table.
+          { $push: { students: studentDetails.name } }
+        );
+        res.send({ message: "Mentor Assigned!!!" });
+      } else {
+        res.send({
+          message: "Student is already assigned to requested mentor",
+        });
+      }
+    } else {
+      res.send({ message: "Mentor is not available" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send({ message: "Error in Connection" });
+  } finally {
+    client.close();
+  }
+});
+
 module.exports = router;
