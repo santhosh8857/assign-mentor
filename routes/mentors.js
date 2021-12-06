f90fdee6f0cca632dbbcfd4a6406430ef17c7d7e
unknown <santhosh8857@gmail.com>
2021-12-06 17:07:15 +0530

    added students to mentor

--- routes/mentors.js
+++ routes/mentors.js
@@ -1,7 +1,7 @@
-const { ObjectId } = require("bson");
 var express = require("express");
-const { MongoClient, dbURL } = require("../dbConfig");
 var router = express.Router();
+const { MongoClient, dbURL } = require("../dbConfig");
+const { ObjectId } = require("bson");
 
 // get mentor details
 router.get("/", async (req, res) => {
@@ -40,18 +40,53 @@ router.post("/create-mentor", async (req, res) => {
 });
 
 // assign mentor to mulitple students
-router.put("/assign-mentor/:id", async (req, res) => {
+router.put("/assign-students/:id", async (req, res) => {
   const client = await MongoClient.connect(dbURL);
   try {
-    // const db = client.db("assign-mentor");
-    // const mentorData = await db
-    //   .collection("mentors")
-    //   .updateOne({ _id: ObjectId(req.params.id) }, { $push: {} });
+    const db = client.db("assign-mentor");
+    let flag = 0; // to check the mentor is assigned
+
+    // assign the req arr to studentArr
+    let studentArr = req.body.students;
+
+    // fetch the mentor details by id to assign the mentor name to student collection
+    const mentorDetails = await db
+      .collection("mentors")
+      .findOne({ _id: ObjectId(req.params.id) });
+
+    // iterating through req array (studentArr)
+    studentArr.forEach(async (student) => {
+      // to check whether the student is already assigned to that mentor to avoid duplicates
+      let find = mentorDetails.students.indexOf(student);
+
+      if (find === -1) {
+        // updating the student Array in mentor collection
+        const mentorData = await db
+          .collection("mentors")
+          .updateOne(
+            { _id: ObjectId(req.params.id) },
+            { $push: { students: student } }
+          );
+
+        // updating the mentor name in the student collection
+        const studentData = await db
+          .collection("students")
+          .updateOne(
+            { name: student },
+            { $set: { mentor: mentorDetails.name } }
+          );
+        flag = 1;
+      } else {
+        console.log(`${student} is already assigned to requested mentor`);
+      }
+    });
+
+    // to whether the mentor is assigned or not
+    if (flag === 1) res.send({ message: "Mentor Assigned!!!" });
+    else res.send({ message: "Students already assigned to the mentor" });
   } catch (err) {
     console.log(err);
     res.send({ message: "Error in connection" });
-  } finally {
-    client.close();
   }
 });
 
