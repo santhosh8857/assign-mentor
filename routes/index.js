fcb6c900e4796f7e38e0f70e6260ead572bde80d
unknown <santhosh8857@gmail.com>
2021-12-06 17:31:46 +0530

    added home

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require("express");
 var router = express.Router();
-
+const { dbURL, MongoClient, mongodb } = require("../dbConfig");
+const { ObjectId } = require("mongodb");
 // get home
-router.get("/", (req, res) => {
-  res.send("Main page");
+router.get("/", async (req, res) => {
+  const client = await MongoClient.connect(dbURL);
+
+  try {
+    const db = client.db("assign-mentor");
+    const mentors = await db.collection("mentors").find().toArray();
+    const students = await db.collection("students").find().toArray();
+    res.send({
+      message: "Success",
+      data: {
+        mentors: mentors,
+        students: students,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.send({ message: "Error in connection" });
+  } finally {
+    client.close();
+  }
 });
 
 module.exports = router;
